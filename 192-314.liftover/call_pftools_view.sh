5ae233acaaa2d945b943edb01caf3d41acb145b2
BioproductivityInformaticsResearchTeam <65061550+BioproductivityInformaticsResearchTeam@users.noreply.github.com>
2025-07-31 02:18:36 +0900

    Delete 192-314.liftover/call_pftools_view.sh

--- 192-314.liftover/call_pftools_view.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# pftools view 実行
-paftools.js view -f aln Bd1_t314-q192.cs_long.paf > Bd1_t314-q192.cs_long.paf.aln
-paftools.js view -f aln Bd2_t314-q192.cs_long.paf > Bd2_t314-q192.cs_long.paf.aln
-paftools.js view -f aln Bd3_t314-q192.cs_long.paf > Bd3_t314-q192.cs_long.paf.aln
-paftools.js view -f aln Bd4_t314-q192.cs_long.paf > Bd4_t314-q192.cs_long.paf.aln
-paftools.js view -f aln Bd5_t314-q192.cs_long.paf > Bd5_t314-q192.cs_long.paf.aln
